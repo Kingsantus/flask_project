834c8ef34f7ece30e8fd38c040de83d927b0820f
kingsantus <nkingsantus@gmail.com>
2023-12-27 08:51:23 +0100

    validating user input

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 REGISTERS = {}
 
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Ultimate Frisbee"
+]
+
 """@app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
@@ -13,13 +19,17 @@ def index():
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 
 @app.route("/register", methods=["POST"])
 def register():
     name = request.form.get("name")
+    if not name:
+        return render_template('failure.html')
     sport = request.form.get("sport")
+    if sport not in SPORTS:
+        return render_template('failure.html')
     REGISTERS[name] = sport
     return render_template("success.html")
 
