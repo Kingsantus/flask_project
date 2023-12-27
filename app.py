02d2ff8938d0a44f64d26f2c9d67083f42a4401e
kingsantus <nkingsantus@gmail.com>
2023-12-27 08:06:33 +0100

    method check

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
+"""@app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template('index.html')
+        return render_template('index1.html')
     elif request.method == "POST":
-        return render_template("greet.html", name=request.form.get("name", "World"))
+        return render_template("greet.html", name=request.form.get("name", "World"))"""
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    return render_template("success.html")
