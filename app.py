5d624ae10d7e3061262915014b98843060132224
kingsantus <nkingsantus@gmail.com>
2023-12-27 08:24:37 +0100

    working with dictionary

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+REGISTERS = {}
+
 """@app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
@@ -16,4 +18,12 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    REGISTERS[name] = sport
     return render_template("success.html")
+
+
+@app.route("/registers")
+def registers():
+    return render_template("registers.html", registers=REGISTERS)
